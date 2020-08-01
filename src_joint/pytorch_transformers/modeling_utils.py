4776f5a4d0a656117127ead1587367763da80b84
Leo <karta09104@gmail.com>
2020-08-01 20:45:17 +0800

    fix issues: Trying to create tensor with negative dimension

--- src_joint/pytorch_transformers/modeling_utils.py
+++ src_joint/pytorch_transformers/modeling_utils.py
@@ -194,7 +194,7 @@ class PretrainedConfig(object):
     @classmethod
     def from_dict(cls, json_object):
         """Constructs a `Config` from a Python dictionary of parameters."""
-        config = cls(vocab_size_or_config_json_file=-1)
+        config = cls()
         for key, value in json_object.items():
             config.__dict__[key] = value
         return config
